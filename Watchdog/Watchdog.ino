b81042acd147296675bb487a864c98bb81761a73
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-11-08 18:56:18 +0100

    feat: update pins

--- Watchdog/Watchdog.ino
+++ Watchdog/Watchdog.ino
@@ -3,16 +3,17 @@
 // Sensors
 const float min_diff = 15.0; // Minimum difference to trigger (in cm)
 const unsigned long timeout = 750; // Timeout (in ms)
+const unsigned long entrance_timeout = 500; // Timeout for entrance (in ms)
 // First sensor
 const byte trig_pin_1 = 12;
 const byte echo_pin_1 = 13;
 // Second sensor
-const byte trig_pin_2 = 42;
-const byte echo_pin_2 = 43;
+const byte trig_pin_2 = 22;
+const byte echo_pin_2 = 23;
 // 7 Segment Display
 const byte num_digits = 4; // Amount of digits
-const byte digit_pins[4] = {2, 3, 4, 5}; // Pins for digits
-const byte segment_pins[8] = {6, 7, 8, 9, 10, 11, 12, 13}; // Pins for segments
+const byte digit_pins[4] = {53, 46, 45, 30}; // Pins for digits
+const byte segment_pins[8] = {50, 42, 34, 38, 41, 49, 33, 37}; // Pins for segments [..., DP]
 // LED
 const byte green_led_pin = 20;
 const byte red_led_pin = 21;
@@ -22,12 +23,12 @@ float base_distance_1;
 float base_distance_2;
 unsigned long previous_1 = 0;
 unsigned long previous_2 = 0;
+unsigned long previous_entrance = 0;
 int people_inside = 0;
 int people_entered = 0;
 
 void setup() {
     Serial.begin(9600);
-    Serial.println();
     sevseg.begin(COMMON_CATHODE, num_digits, digit_pins, segment_pins);
     sevseg.setBrightness(90);
     base_distance_1 = read_sensor(trig_pin_1, echo_pin_1);
@@ -45,6 +46,9 @@ void loop() {
     bool active_1 = difference_1 >= min_diff;
     bool active_2 = difference_2 >= min_diff;
     unsigned long time = millis();
+    if (time - previous_entrance <= entrance_timeout) {
+        return;
+    }
     if (active_1) {
         previous_1 = time;
     } else if (active_2) {
@@ -54,7 +58,8 @@ void loop() {
     if (active_1 && time - previous_2 <= timeout) {
         --people_inside;
         previous_2 = 0;
-        Serial.println("-");
+        previous_entrance = time;
+        Serial.println("-SIG[START]");
         digitalWrite(green_led_pin, LOW);
         digitalWrite(red_led_pin, HIGH);
     }
@@ -63,7 +68,8 @@ void loop() {
         ++people_inside;
         ++people_entered;
         previous_1 = 0;
-        Serial.println("+");
+        previous_entrance = time;
+        Serial.println("+SIG[START]");
         digitalWrite(green_led_pin, HIGH);
         digitalWrite(red_led_pin, LOW);
     }
